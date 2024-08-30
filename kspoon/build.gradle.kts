0ffa241d8fc13eae74a3e8f022bc0443d68f6d48
Bruno Wieczorek <bruno.wieczorek@gmail.com>
2024-08-30 19:24:28 +0200

    Add kotlin targets to :kspoon

--- kspoon/build.gradle.kts
+++ kspoon/build.gradle.kts
@@ -1,6 +1,38 @@
+import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.kotlinx.serialization)
+}
+
+group = "dev.burnoo.kspoon"
+version = "0.0.1-SNAPSHOT"
+
+kotlin {
+    jvm()
+
+    js {
+        browser()
+        nodejs()
+    }
+    @OptIn(ExperimentalWasmDsl::class)
+    wasmJs {
+        browser()
+        nodejs()
+        d8()
+    }
+
+    linuxX64()
+    linuxArm64()
+    tvosArm64()
+    tvosX64()
+    tvosSimulatorArm64()
+    macosX64()
+    macosArm64()
+    iosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    mingwX64()
 }
 
 kotlin.sourceSets.commonMain {
